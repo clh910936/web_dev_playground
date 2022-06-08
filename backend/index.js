0dc6cc78ce85bd9ca6206d01476c99a1f6e4d281
Carrie Hunner <carrielhunner@gmail.com>
2022-06-08 14:30:29 -0500

    frontend too

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    next();
-});
+const corsOpts = {
+    origin: '*',
+
+    methods: [
+        'GET',
+        'PUT',
+    ],
+
+    allowedHeaders: [
+        'Content-Type',
+    ],
+};
+
+app.use(cors(corsOpts));
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     next();
+// });
 
 app.get('/', (req, res) => {
     const data = {response: "yes"};
